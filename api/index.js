a3edfb3af020635ef601848f3dd981512e5fb9e0
perudev3 <peru.dev3@gmail.com>
2026-02-06 02:29:26 -0500

    cambios requeridos

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { askGemini } from "./chat.js";
+import { askGemini } from "../chat.js";
 
 dotenv.config();
 
